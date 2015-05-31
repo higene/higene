0cd40f3cbd7cd79d2e98041fa96a2809b2b03f80
Jian-Long Huang <jlhgln@gmail.com>
2015-05-31 15:47:42 +0800

    Workspaces should be retrieved from the member objects; add the description parameter to workspace creation

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -2,7 +2,8 @@ class WorkspacesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    workspaces = Workspace.where("members.user" => current_user.id)
+    members = Member.where(user: current_user)
+    workspaces = members.collect(&:workspace)
     render json: workspaces
   end
 
@@ -11,7 +12,9 @@ class WorkspacesController < ApplicationController
 
   def create
     member = Member.new(role: :owner, user: current_user)
-    workspace = Workspace.new(name: params[:name], members: [member])
+    workspace = Workspace.new name: params[:name],
+                              description: params[:description],
+                              members: [member]
     member.save!
     workspace.save!
     render json: workspace
