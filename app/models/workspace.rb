a796c82bea99d5aa550802802c9142b648eaa426
Jian-Long Huang <jlhgln@gmail.com>
2015-06-13 23:50:28 +0800

    Add a before_destroy callback to remove all the workspace's contents

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,4 +1,17 @@
 class Workspace < ActiveRecord::Base
   has_many :members, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  before_destroy :destroy_data
+
+  private
+
+  def destroy_data
+    [Sequence, Location].each do |klass|
+      cmd = %(
+        DELETE FROM #{klass.table_name}
+               WHERE workspace_id = '#{id}'
+      ).squish
+      klass.connection.execute(cmd)
+    end
+  end
 end
