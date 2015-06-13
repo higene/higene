355b34735e7f7ef245e209e3be445056feec448a
Jian-Long Huang <jlhgln@gmail.com>
2015-06-13 22:58:21 +0800

    Change the path prefix of workspace actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources 'workspaces', path: 's', format: false do
+  resources 'workspaces', path: 'w', format: false do
     resources 'sequences', param: :name,
                            format: false,
                            constraints: { name: %r{[^\/]+} }
