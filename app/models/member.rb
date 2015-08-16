9988445ea771a47eb678a8d8be692dea937d0c70
Jian-Long Huang <jlhgln@gmail.com>
2015-08-16 20:51:16 +0800

    Member model: add "role" field validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,6 @@
 class Member < ActiveRecord::Base
   belongs_to :user
   belongs_to :workspace
+  validates :role, presence: true,
+                   inclusion: { in: %w(owner editor viewer) }
 end
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:bill)
+    @workspace = Workspace.new name: "new workspace"
+  end
+
+  test "should save when owner role" do
+    member = Member.new workspace: @workspace,
+                        user: @user,
+                        role: "owner"
+    assert member.save
+  end
+
+  test "should save when editor role" do
+    member = Member.new workspace: @workspace,
+                        user: @user,
+                        role: "editor"
+    assert member.save
+  end
+
+  test "should save when viewer role" do
+    member = Member.new workspace: @workspace,
+                        user: @user,
+                        role: "viewer"
+    assert member.save
+  end
+
+  test "should not save when invalid role" do
+    member = Member.new workspace: @workspace,
+                        user: @user,
+                        role: "admin"
+    assert_not member.save
+  end
+
+  test "should not save when role is not present" do
+    member = Member.new workspace: @workspace,
+                        user: @user
+    assert_not member.save
+  end
 end
